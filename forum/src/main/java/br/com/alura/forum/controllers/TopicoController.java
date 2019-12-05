e2c1f76b3b919cc9ef1ca2eef2c3f87ea6aee88b
Rafael de Mesquita Moura <rafael.mesquita@softcube.com.br>
2019-12-05 07:38:42 -0300

    Criação do TopicoDTO para a camada de serviço

--- forum/src/main/java/br/com/alura/forum/controllers/TopicoController.java
+++ forum/src/main/java/br/com/alura/forum/controllers/TopicoController.java
@@ -3,22 +3,21 @@ package br.com.alura.forum.controllers;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controllers.dtos.TopicoDTO;
 import br.com.alura.forum.models.Curso;
 import br.com.alura.forum.models.Topico;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@RestController
 public class TopicoController {
 	
-	@ResponseBody
 	@RequestMapping("/topicos")	
-	public List<Topico> topicos() {
+	public List<TopicoDTO> topicos() {
 		Topico topico = new Topico("Dúvida", "Dúvida no Curso de Spring Boot", new Curso("Spring Boot", "Programação"));
 		
-		return Arrays.asList(topico, topico, topico);
+		return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
 	}
 
 }
