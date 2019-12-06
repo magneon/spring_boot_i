e93408d3cd9c17a5b8b336475cb77f9177c0b237
Rafael de Mesquita Moura <rafael.mesquita@softcube.com.br>
2019-12-06 07:34:14 -0300

    Implementação do Repository do Spring Data

--- forum/src/main/java/br/com/alura/forum/controllers/TopicoController.java
+++ forum/src/main/java/br/com/alura/forum/controllers/TopicoController.java
@@ -1,23 +1,31 @@
 package br.com.alura.forum.controllers;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controllers.dtos.TopicoDTO;
-import br.com.alura.forum.models.Curso;
 import br.com.alura.forum.models.Topico;
+import br.com.alura.forum.repositories.TopicoRepository;
 
 @RestController
 public class TopicoController {
 	
+	@Autowired
+	private TopicoRepository repositoryTopico;
+	
 	@RequestMapping("/topicos")	
-	public List<TopicoDTO> topicos() {
-		Topico topico = new Topico("Dúvida", "Dúvida no Curso de Spring Boot", new Curso("Spring Boot", "Programação"));
-		
-		return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
+	public List<TopicoDTO> topicos(String nomeCurso) {
+		List<Topico> topicos = null;
+		if (nomeCurso == null) {
+			topicos = repositoryTopico.findAll();
+		} else {
+			topicos = repositoryTopico.findByCursoNome(nomeCurso);
+//			topicos = repositoryTopico.pegaPorCursoNome(nomeCurso);
+		}
+		return TopicoDTO.converter(topicos);
 	}
 
 }
